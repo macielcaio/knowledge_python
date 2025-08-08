5da8411895693c56d4af9ac6154fea35f9f32340
caio.maciel <caio.maciel@e-unicred.com.br>
2025-08-08 17:27:32 -0300

    loops.py - treinando uso de loops

--- loops.py
+++ loops.py
@@ -10,3 +10,24 @@
 #for i in range(3):
 #  print("A")
 #print("B")
+# while teste de loop com break, continue e pass
+temp_f = 40
+while temp_f > 32:
+    print(f"Thw water is {temp_f} degress")
+    temp_f -= 1
+    if temp_f == 33:
+        break
+
+# using continue in a loop for
+for i in range(1,11):
+    if i == 7:
+        print("we skipped 7")
+        continue
+    print(f"this is the number {i}")
+
+# using pass in a loop for
+for i in range(1,11):
+    if i == 3:
+        pass
+    else:
+        print(f"this is the number {i}")
